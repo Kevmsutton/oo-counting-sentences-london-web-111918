32480099141c62c32bcd3882a14cb2ecc2162a20
kevmsutton <kevmsutton@gmail.com>
2018-11-22 18:29:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
+
+  # def count_sentences
+  # self.split("." || "?" || "!").length
+  # end
 
   def count_sentences
+  count = 0
+  array = self.split(" ")
+  array.each {|word| count += 1 if word.end_with?("!", "?", ".")}
+  count
+end
 
-  end
 end
